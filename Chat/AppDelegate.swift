eeebfc71393fb415f2a9be7a6fe6fc25dfb2a9d5
Ben Whittle <benwhittle31@gmail.com>
2020-12-03 13:35:00 -0600

    Initial Commit

---
+++ Chat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Chat
+//
+//  Created by Ben Whittle on 12/3/20.
+//  Copyright © 2020 Ben Whittle. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Chat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Ben Whittle on 12/3/20.
+//  Copyright © 2020 Ben Whittle. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
