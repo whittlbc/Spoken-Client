11ce37952783cc33078e86f720c2b3cc565f9d1a
Ben Whittle <benwhittle31@gmail.com>
2020-12-10 13:06:06 -0600

    add Workspace model

---
+++ Chat/Extensions/Workspace+Arrow.swift
@@ -0,0 +1,21 @@
+//
+//  Workspace+Arrow.swift
+//  Chat
+//
+//  Created by Ben Whittle on 12/10/20.
+//  Copyright © 2020 Ben Whittle. All rights reserved.
+//
+
+import Foundation
+import Arrow
+import Networking
+
+extension Workspace: ArrowParsable {
+    
+    public mutating func deserialize(_ json: JSON) {
+        uid <-- json["uid"]
+        name <-- json["name"]
+    }
+}
+
+extension Workspace: NetworkingJSONDecodable {}
---
+++ Chat/Models/Workspace.swift
@@ -0,0 +1,14 @@
+//
+//  Workspace.swift
+//  Chat
+//
+//  Created by Ben Whittle on 12/10/20.
+//  Copyright © 2020 Ben Whittle. All rights reserved.
+//
+
+import Foundation
+
+struct Workspace: Identifiable {
+    var uid = ""
+    var name = ""
+}
